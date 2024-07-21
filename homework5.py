09e1ebcbdf58c6b0289cc3298f535b1099fe5a17
Timofey186 <popov.timofey.my@gmail.com>
2024-07-21 17:51:28 +0500

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,5 +1,6 @@
 immutable_var = (1, 2, 'a', 'b', True)
 print(immutable_var)
+immutable_var.extend ("c") # кортеж является не изменяемымы объектом а используется он как хранилище
 mutable_list = [1, 2, 'a', 'b']
 mutable_list.extend([True])
 print(mutable_list)
