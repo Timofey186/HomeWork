2585c11e344cd598b9bd8ae88c2f3393b22baa9b
Timofey186 <popov.timofey.my@gmail.com>
2024-07-26 21:26:53 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,13 @@
+number = int(input('Введите число'))
+number1 = int(input('Введите второе число'))
+number2 = int(input('Введите третье число'))
+if number == number1 :
+    print(2)
+elif number == number2 :
+    print(2)
+elif number1 == number2 :
+    print(2)
+elif number == number1 == number2 :
+    print(3)
+else:
+    print(0)
