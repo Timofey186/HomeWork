6eb0fcf2a6f006e618d7fe12eb0bf8c42dffd76f
Timofey186 <popov.timofey.my@gmail.com>
2024-07-26 21:39:32 +0500

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,13 +1,13 @@
-number = int(input('Введите число'))
-number1 = int(input('Введите второе число'))
-number2 = int(input('Введите третье число'))
-if number == number1 :
+first = int(input('first'))
+second = int(input('second'))
+third = int(input('third'))
+if first == second :
     print(2)
-elif number == number2 :
+elif first == third :
     print(2)
-elif number1 == number2 :
+elif second == third :
     print(2)
-elif number == number1 == number2 :
+elif first == second == third :
     print(3)
 else:
     print(0)
